cc415531b4db738547d76c8088acc25fd29c0050
Ashwin Narkar <ashwin.narkar@gmail.com>
2017-10-29 15:13:57 -0700

    Added Teensy Receiver Code
    Testing to send one character from the Arduino to the Teensy

---
+++ wirelessSpaceCommTeensy/wirelessSpaceCommTeensy.ino
@@ -0,0 +1,31 @@
+#include <nRF24L01.h>
+#include <printf.h>
+#include <RF24.h>
+#include <RF24_config.h>
+
+#define CE 15
+#define CS 10
+
+char receivedLetter = 'u';
+RF24 radiomodule(CE,CS);
+
+void setup() {
+  // put your setup code here, to run once:
+  radiomodule.begin();
+  radiomodule.setChannel(0b00000110);
+  radiomodule.setPALevel(RF24_PA_MIN);
+  radiomodule.openReadingPipe(1,0xc2c2c2c2c2);
+  radiomodule.openWritingPipe(0xe7e7e7e7e7);
+  radiomodule.setCRCLength(RF24_CRC_16);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  radiomodule.startListening();
+  if(radiomodule.available()){
+    radiomodule.read(&receivedLetter, sizeof(receivedLetter));
+  }
+  radiomodule.stopListening();
+  Serial.println(receivedLetter);
+}
