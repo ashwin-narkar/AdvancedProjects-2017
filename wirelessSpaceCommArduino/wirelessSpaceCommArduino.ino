8661f3e13bb0aae6e72fc03df08bee8256f0528a
Kenny Chan <chankenny900@gmail.com>
2017-10-29 18:18:47 -0700

    Update wirelessSpaceCommArduino.ino

--- wirelessSpaceCommArduino/wirelessSpaceCommArduino.ino
+++ wirelessSpaceCommArduino/wirelessSpaceCommArduino.ino
@@ -10,36 +10,42 @@
 #define CE 9
 
 
-RF24 transmitter;
+RF24 transmitter(CE, CSN);
 bool transmitting;
-char c;
+char c[1];
 
 
 void setup() {
   // put your setup code here, to run once:
-  transmitter(CE, CSN);
   pinMode(CSN, OUTPUT);
   pinMode(MOSI, OUTPUT);
   pinMode(MISO, INPUT);
   pinMode(SCK, OUTPUT);
   pinMode(CE, OUTPUT);
+
+  Serial.begin(9600); 
   transmitter.begin();
   transmitter.setChannel(6);
   transmitter.setPALevel(RF24_PA_MIN);
   uint8_t writeAddress[] = {0xC2,0xC2,0xC2,0xC2,0xC2};
   uint8_t readAddress[] = {0xe7,0xe7,0xe7,0xe7,0xe7};
-  transmitter.openWritingPipe(1,address);
-  transmitter.openReadingPipe(2,readAddress);
+  transmitter.openWritingPipe(writeAddress);
+  //transmitter.openReadingPipe(1,readAddress);
   transmitting = true;
-  c = 'a';
+  c[0] = 'a';
+
+  
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  Serial.println("Working");
   if (transmitting) {
-    transmitter.write(&c,1);
+    transmitter.write(c,1);
+    Serial.println("sent");
   }
-  transmitting = false;
-  while (true);
+  //Serial.println('a');
+  //transmitting = false;
+  //while (true);
 
 }
