0d0f09c33cd72ccf1ed7a375ff69aece02db169b
Ashwin Narkar <ashwin.narkar@gmail.com>
2017-10-30 16:09:38 -0700

    Updated Teensy Code

--- wirelessSpaceCommTeensy/wirelessSpaceCommTeensy.ino
+++ wirelessSpaceCommTeensy/wirelessSpaceCommTeensy.ino
@@ -7,15 +7,17 @@
 #define CE 9
 #define CS 10
 #define green 20
-
 char r[] = {'u'};
+char *rPtr = r;
 RF24 radiomodule(CE,CS);
 bool received = false;
+uint8_t i = 0;
+uint8_t *p = &i;
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode(CE, INPUT);
-  pinMode(CS, INPUT);
+  pinMode(CE, OUTPUT);
+  pinMode(CS, OUTPUT);
   pinMode(MOSI, INPUT);
   pinMode(MISO, OUTPUT);
   pinMode(SCK, INPUT);
@@ -24,12 +26,13 @@ void setup() {
   Serial.begin(9600);
   
   radiomodule.begin();
-  radiomodule.setChannel(0b00000110);
+  radiomodule.setChannel(6);
   radiomodule.setPALevel(RF24_PA_MIN);
+  radiomodule.setAddressWidth(5);
   uint8_t writeAddress[] = {0xe7,0xe7,0xe7,0xe7,0xe7};
   uint8_t readAddress[] = {0xC2,0xC2,0xC2,0xC2,0xC2};
   radiomodule.openReadingPipe(1,readAddress);
-  radiomodule.openWritingPipe(writeAddress);
+  //radiomodule.openWritingPipe(writeAddress);
   radiomodule.setCRCLength(RF24_CRC_16);
   
   
@@ -37,21 +40,26 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  radiomodule.startListening();
-  digitalWrite(green, HIGH);
-  delay(1000);
-  digitalWrite(green, LOW);
-  delay(1000);
-  if(radiomodule.available() && !received){
-    Serial.println("Reading");
-    radiomodule.read(r, sizeof(r));
-    received = true;
-  }
-  else {
-    Serial.println("Not available");
-  }
-  if (received == true) {
-    radiomodule.stopListening();
-  }
-  Serial.println(r[0]);
+//       delay(1000);
+//   Serial.println("Reading");
+// 
+//    Serial.println((int) r[0]);
+    radiomodule.startListening();
+//  digitalWrite(green, HIGH);
+//  delay(1000);
+//  digitalWrite(green, LOW);
+//  delay(1000);
+    
+    while (radiomodule.available(p)) {
+      Serial.println("Available");
+      radiomodule.read(rPtr, 2); 
+    }
+    radiomodule.printDetails();
+    Serial.println();
+    Serial.println();
+    
 }
+     
+     
+
+
