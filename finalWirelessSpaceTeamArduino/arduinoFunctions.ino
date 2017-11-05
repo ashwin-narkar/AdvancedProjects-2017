c1f4c50a119ffdc4d3f158c4503739e2fb975197
Kenny Chan <chankenny900@gmail.com>
2017-11-05 14:45:50 -0800

    Update arduinoFunctions.ino

--- finalWirelessSpaceTeamArduino/arduinoFunctions.ino
+++ finalWirelessSpaceTeamArduino/arduinoFunctions.ino
@@ -1,26 +1,4 @@
 void risingEdgeDetect(){
-
-//  if(digitalRead(yellowButton) != 0){
-//    risingEdgeYellow = true;
-//    Serial.println("Here1");
-//  }
-//  if(digitalRead(blueButton) != 0){
-//    risingEdgeBlue = true;
-//    Serial.println("Here2");
-//  }
-//  if(digitalRead(greenButton) != 0){
-//    risingEdgeGreen = true;
-//    Serial.println("Here3");
-//  }
-//  if(digitalRead(yellowButton) == 0){
-//    risingEdgeYellow = false;
-//  }
-//  if(digitalRead(blueButton) == 0){
-//    risingEdgeBlue = false;
-//  }
-//  if(digitalRead(greenButton) == 0){
-//    risingEdgeGreen = false;
-//  }
   
   if(digitalRead(yellowButton) != 0){
     previousReadingYellow = risingEdgeYellow;
@@ -54,19 +32,6 @@ void risingEdgeDetect(){
 void listenForPress(){
   risingEdgeDetect();
 
-//  if(risingEdgeYellow){
-//    buttonPushed = 'Y';
-//    Serial.println("Yellow Pressed");
-//  }
-//  if(risingEdgeBlue){
-//    buttonPushed = 'B';
-//    Serial.println("Blue Pressed");
-//  }
-//  if(risingEdgeGreen){
-//    buttonPushed = 'G';
-//    Serial.println("Green Pressed");
-//  }
-  
   if(risingEdgeYellow && !previousReadingYellow){
     buttonPushed = 'Y';
     //Serial.println("Yellow Pressed");
@@ -80,16 +45,6 @@ void listenForPress(){
     //Serial.println("Green Pressed");
   }
 }
-
-/*void addPress(){
-//  radio.stopListening();
-//  Serial.println(radio.write(&buttonPushed,sizeof(buttonPushed)));
-//  buttonPushed = 'z';
-//  radio.startListening();
-  Player.combo += buttonPushed;
-  buttonPushed = 'z';
-  i++;
-}*/
 
 bool checkSeq(unsigned char player, unsigned char correct) {
   if (player == correct){
