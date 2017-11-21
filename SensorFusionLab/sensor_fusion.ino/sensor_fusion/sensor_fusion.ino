81698787ad75caecaa41a97f7f362dde485c0f56
Kenny Chan <chankenny900@gmail.com>
2017-11-21 10:59:44 -0800

    Finalized Code

--- SensorFusionLab/sensor_fusion.ino/sensor_fusion/sensor_fusion.ino
+++ SensorFusionLab/sensor_fusion.ino/sensor_fusion/sensor_fusion.ino
@@ -4,14 +4,34 @@
 
 int xAccelBias,yAccelBias,zAccelBias,xGyroBias,yGyroBias,zGyroBias;
 
+//variables for awkward online function
+float angle_x, angle_y, angle_x_acc, angle_y_acc = 0;
+long total_accel_vector = 0;
+float angle_x_output, angle_y_output = 0;
+long loop_timer = 0;
 
+//variables for quaterion
+float magnitude = 0;
+float currentAngle, pastAngle, deltaAngle= 0;
+float deltaTime = 0;
+bool first_time_run = false;
+
+float accelWeight = 0.5;
 
 struct vector accel;
 struct vector gyro;
+struct vector filteredVect;
+struct quaternion quater;
+struct vector gyroOrientation;
 
 void setup() {
   setupRegisters();  
   biasCorrect();
+  //loop_timer = micros();
+  deltaTime = millis();
+  gyroOrientation.x = 0;
+  gyroOrientation.y = 0;
+  gyroOrientation.z = 1;
 }
 
 void updateAccelVector() {
@@ -32,8 +52,8 @@ void printVector(struct vector v) {
   Serial.print(" ");
   Serial.print(v.y);
   Serial.print(" ");
-  Serial.println(v.z);
-  
+  Serial.print(v.z);
+  Serial.print(" ");
 }
 
 void loop() {
@@ -45,8 +65,17 @@ void loop() {
     //Serial.println();
     //Serial.println();
     //Serial.print("Gyro: ");
-    //updateGyroVector();
+    updateGyroVector();
     //printVector(gyro);
-    //delay(1000);
+    
+
+    quaternionator(&gyroOrientation);
+    quaternionator(&filteredVect);
+    printVector(gyroOrientation);
+    filterAndCombine();
+    printVector(filteredVect);
+    Serial.println();
+    deltaTime = millis();
+    
   }
 }
