deeab8ea7a139220562036f2d6aec48aa98c927f
Ashwin Narkar <ashwin.narkar@gmail.com>
2017-12-04 14:59:30 -0800

    Started Lab3

---
+++ MotorControlLab/MotorControlLab.ino
@@ -0,0 +1,11 @@
+#define motor 3
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(motor) = OUTPUT;
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
